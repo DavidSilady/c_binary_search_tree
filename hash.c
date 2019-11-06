aab3b9edd04ed247325d9a8506caa5f6d3778da1
DavidSilady <david.silady@gmail.com>
2019-11-06 10:59:19 +0100

    added hash resizing

--- hash.c
+++ hash.c
@@ -6,6 +6,10 @@
 #include "tree.h"
 #include <stdlib.h>
 
+#define MAX_LOAD_FACTOR 1.5
+
+void resize(Hash *hash, int new_size);
+
 Tree **new_hash_table(int size) {
     Tree **table = malloc(size * sizeof(Tree));
     for (int i = 0; i < size; ++i) {
@@ -27,6 +31,29 @@ int hash_function(Hash *hash, int value) {
     return value % hash->prime;
 }
 
+int find_next_prime(int value) {
+    int next_prime = value;
+
+    if(next_prime % 2 == 0) {
+        next_prime += 1;
+    }
+
+    while(1) {
+        int i;
+        for (i = 3; i < next_prime; i += 2) {
+            if(next_prime % 2 == 0) {
+                next_prime += 2;
+                break;
+            }
+        }
+        if (i == next_prime) {
+            break;
+        }
+    }
+
+    return next_prime;
+}
+
 void hash_add(Hash *hash, int value) {
     int index = hash_function(hash, value);
     Tree *tree = hash->table[index];
@@ -35,6 +62,10 @@ void hash_add(Hash *hash, int value) {
     insert(tree, value);
     hash->num_elements += tree->size - old_size;
     hash->loadFactor = ((double) hash->num_elements / (double) hash->prime);
+
+    if(hash->loadFactor > 1.5) {
+        resize(hash, find_next_prime(hash->prime * 2));
+    }
 }
 
 Node *hash_find(Hash *hash, int value) {
@@ -42,3 +73,29 @@ Node *hash_find(Hash *hash, int value) {
     Tree *tree = hash->table[index];
     return find_value(tree, value);
 }
+
+void hash_refill(Hash *hash, Node *node) {
+    if (node == NULL)
+        return;
+
+    hash_refill(hash, node->left);
+    hash_refill(hash, node->right);
+
+    hash_add(hash, node->value);
+    free(node);
+}
+
+void resize(Hash *hash, int new_size) {
+    Tree **old_table = hash->table;
+    int old_prime = hash->prime;
+
+    hash->table = new_hash_table(new_size);
+    hash->prime = new_size;
+
+    for (int i = 0; i < old_prime; ++i) {
+        hash_refill(hash, old_table[i]->root);
+    }
+
+    free(old_table);
+}
+
